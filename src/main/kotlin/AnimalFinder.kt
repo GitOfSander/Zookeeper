fe5600a4df63b5fefdd41618fc05bcfa89362273
sanderpals <sanderpals@live.nl>
2023-03-05 16:49:45 +0100

    Changed code to after both outputs divided lines return

--- src/main/kotlin/AnimalFinder.kt
+++ src/main/kotlin/AnimalFinder.kt
@@ -4,10 +4,11 @@ class AnimalFinder {
     fun findByNumber(input: String) {
         val animalNumber = input.toIntOrNull()
         if (animalNumber == null || animalNumber !in animals.getRange()) {
-            return println("Please enter a number of a habitat that exist!")
+            println("Please enter a number of a habitat that exist!")
+        } else {
+            println(animals.getAnimal(animalNumber))
         }
 
-        println(animals.getAnimal(animalNumber))
         println()
         println("---------------------")
         println()
